80c61a5038b18ea863276edc163126d43c4f5c07
JackRuhala <ruhalaja@msu.edu>
2024-10-18 10:24:18 -0400

    Beer_Analysis_V3.py

--- Streamlit Beer Page files/Beer_Analysis_V3.py
+++ Streamlit Beer Page files/Beer_Analysis_V3.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import math
-import altair as alt
 import plotly.express as px
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.decomposition import PCA
 ###-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------###
 
