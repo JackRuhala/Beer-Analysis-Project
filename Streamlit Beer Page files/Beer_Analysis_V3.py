a71dcfd5bc1e4ab35cabe0a59c1d35cc839dee44
JackRuhala <ruhalaja@msu.edu>
2024-10-18 10:57:11 -0400

    Beer_Analysis_V3.py

--- Streamlit Beer Page files/Beer_Analysis_V3.py
+++ Streamlit Beer Page files/Beer_Analysis_V3.py
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip install -r Streamlit Beer Page files/requirements.txt
 ###-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------###
 import streamlit as st
 import pandas as pd
